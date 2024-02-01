52326f4bd07cc80ad236960cf0fb01c27615b663
Cheng Shi <sc971008@gmail.com>
2024-02-01 15:49:49 -0500

    button

--- scripts/manipulatedom.js
+++ scripts/manipulatedom.js
@@ -20,12 +20,30 @@ function createTxtNode(txt) {
   let txtNode = document.createTextNode(txt);
   return txtNode;
 }
+function createButtonNode(txt,event,listener){
+  let btnNode = document.createElement("button");
+  let btnTextNode = document.createTextNode(txt);
+  btnNode.appendChild(btnTextNode);
+  btnNode.addEventListener(event,listener);
+  return btnNode;
+}
+
+function edit(node){
+  let newNode = document.createElement("input");
+  newNode.type= 'text';
+  newNode.placeholder = "Enter Cell (x,y)"
+  node.replaceChild(newNode,node.childNodes[0]);
+}
+
 
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createButtonNode("Edit Text",'click', function(){
+      edit(col1); 
+    });
+    tableNode.appendChild(createTRNode([col1,col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
